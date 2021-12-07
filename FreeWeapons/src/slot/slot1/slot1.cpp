5f48f1212daaf7646182f8e57796fef4d1952778
Huga22118 <huga.ghaisan@gmail.com>
2021-12-08 00:20:32 +0700

    Added Rocket Launcher for Slot 1

--- FreeWeapons/src/slot/slot1/slot1.cpp
+++ FreeWeapons/src/slot/slot1/slot1.cpp
@@ -18,6 +18,7 @@ public:
 			if (hugav1 && KeyPressed(VK_F8) && CTimer::m_snTimeInMilliseconds - keyPressTime > 50)
 			{
 			CStreaming::RequestModel(MODEL_AK47, 2);
+			CStreaming::RequestModel(MODEL_ROCKETLA, 2);
 			CStreaming::RequestModel(MODEL_MICRO_UZI, 2);
 			CStreaming::RequestModel(MODEL_COLT45, 2);
 			CStreaming::RequestModel(MODEL_CHROMEGUN, 2);
@@ -26,11 +27,14 @@ public:
 			hugav1->GiveWeapon(WEAPON_MICRO_UZI, 1000, true);
 			hugav1->GiveWeapon(WEAPON_PISTOL, 1000, true);
 			hugav1->GiveWeapon(WEAPON_SHOTGUN, 1000, true);
+			hugav1->GiveWeapon(WEAPON_RLAUNCHER, 1000, true);
 			hugav1->SetCurrentWeapon(WEAPON_AK47);
 			hugav1->SetCurrentWeapon(WEAPON_PISTOL);
 			hugav1->SetCurrentWeapon(WEAPON_MICRO_UZI);
 			hugav1->SetCurrentWeapon(WEAPON_SHOTGUN);
+			hugav1->SetCurrentWeapon(WEAPON_RLAUNCHER);
 			CStreaming::SetModelIsDeletable(MODEL_AK47);
+			CStreaming::SetModelIsDeletable(MODEL_ROCKETLA);
 			CStreaming::SetModelIsDeletable(MODEL_COLT45);
 			CStreaming::SetModelIsDeletable(MODEL_MICRO_UZI);
 			CStreaming::SetModelIsDeletable(MODEL_CHROMEGUN);
