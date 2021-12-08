16e87ccc3dbee32c02470e9976eb0ffcd4949c9b
Huga22118 <huga.ghaisan@gmail.com>
2021-12-08 12:45:35 +0700

    Added Country Rifle and Decreased KeyPressTime to 25

--- FreeWeapons/src/slot/slot1/slot1.cpp
+++ FreeWeapons/src/slot/slot1/slot1.cpp
@@ -15,29 +15,33 @@ public:
 		Events::gameProcessEvent += []
 		{
 			CPed *hugav1 = FindPlayerPed();
-			if (hugav1 && KeyPressed(VK_F8) && CTimer::m_snTimeInMilliseconds - keyPressTime > 50)
+			if (hugav1 && KeyPressed(VK_F8) && CTimer::m_snTimeInMilliseconds - keyPressTime > 25)
 			{
 			CStreaming::RequestModel(MODEL_AK47, 2);
 			CStreaming::RequestModel(MODEL_ROCKETLA, 2);
 			CStreaming::RequestModel(MODEL_MICRO_UZI, 2);
 			CStreaming::RequestModel(MODEL_COLT45, 2);
 			CStreaming::RequestModel(MODEL_CHROMEGUN, 2);
+			CStreaming::RequestModel(MODEL_CUNTGUN, 2);
 			CStreaming::LoadAllRequestedModels(false);
 			hugav1->GiveWeapon(WEAPON_AK47, 1000, true);
 			hugav1->GiveWeapon(WEAPON_MICRO_UZI, 1000, true);
 			hugav1->GiveWeapon(WEAPON_PISTOL, 1000, true);
 			hugav1->GiveWeapon(WEAPON_SHOTGUN, 1000, true);
 			hugav1->GiveWeapon(WEAPON_RLAUNCHER, 1000, true);
+			hugav1->GiveWeapon(WEAPON_COUNTRYRIFLE, 1000, true);
 			hugav1->SetCurrentWeapon(WEAPON_AK47);
 			hugav1->SetCurrentWeapon(WEAPON_PISTOL);
 			hugav1->SetCurrentWeapon(WEAPON_MICRO_UZI);
 			hugav1->SetCurrentWeapon(WEAPON_SHOTGUN);
 			hugav1->SetCurrentWeapon(WEAPON_RLAUNCHER);
+			hugav1->SetCurrentWeapon(WEAPON_COUNTRYRIFLE);
 			CStreaming::SetModelIsDeletable(MODEL_AK47);
 			CStreaming::SetModelIsDeletable(MODEL_ROCKETLA);
 			CStreaming::SetModelIsDeletable(MODEL_COLT45);
 			CStreaming::SetModelIsDeletable(MODEL_MICRO_UZI);
 			CStreaming::SetModelIsDeletable(MODEL_CHROMEGUN);
+			CStreaming::SetModelIsDeletable(MODEL_CUNTGUN);
 		    }
 		};
 	}
