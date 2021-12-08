d4c4977c2943f2414bebf0f6a7e6e96343b388dc
Huga22118 <huga.ghaisan@gmail.com>
2021-12-08 14:38:52 +0700

    Add Slot 3 stuff

---
+++ FreeWeapons/src/slot/slot3/slot3.cpp
@@ -0,0 +1,17 @@
+/*
+	Plugins by Huga
+	Slot 3 Coming soon...
+*/
+#include "plugin.h"
+#include "stuff.h"
+
+using namespace plugin;
+
+class FreeWeaponByHuga3
+{
+public:
+	FreeWeaponByHuga3()
+	{
+
+	}
+} FreeWeaponByHuga3;
